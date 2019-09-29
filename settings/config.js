c5244c67b01ab4d59d9bab352547dbf5eba23ad7
Jasmin Miftari <jasmin.mif@gmail.com>
2019-09-29 23:30:38 +0200

    Documentation on the app urls

--- settings/config.js
+++ settings/config.js
@@ -1,7 +1,12 @@
 const PORT = 3000;
 const serverStartupMessage = `Server is listening on port ${PORT}`;
 
+const mongoUser = 'test';
+const mongoPass = 'test';
+const mongoUrl = `mongodb://${mongoUser}:${mongoPass}@testDb`;
+
 module.exports = {
     PORT,
     serverStartupMessage
+    // mongoUrl
 };
--- settings/urls.js
+++ settings/urls.js
@@ -1,3 +1,9 @@
+/*
+ * This file includes all of the app URLs,
+ * which makes it easier to use on the app.js
+ * and have a cleaner server startup script.
+ */
+
 const userUrls = require('../user/urls');
 
 const routes = app => {
